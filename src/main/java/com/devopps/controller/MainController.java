3028434c2223208496cacacd38c3cbefa29e864a
sunderkrishnaupreti <sunderkrishna_upreti@yahoo.com>
2021-12-05 17:26:05 -0600

    Initial project added to github

---
+++ src/main/java/com/devopps/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.devopps.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "This is home page";
+    }
+}
+
